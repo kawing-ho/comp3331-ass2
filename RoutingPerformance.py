deb55f3fd13ffd320e4c9251d4ae316dca3aa531
z5087077 <z5087077@student.unsw.edu.au>
2017-09-21 00:06:15 +1000

    Started planning out the graph structure

--- RoutingPerformance.py
+++ RoutingPerformance.py
@@ -1,9 +1,69 @@
-#!/bin/usr/python
+#!/usr/bin/python
 #COMP3331 Assignment2 
 #z5087077 Ka Wing Ho 
 #z5113471 Andy Yang 
 
 import sys, re
+
+#Code referrenced from: http://www.bogotobogo.com/python/python_graph_data_structures.php
+#Graph has nodes / Nodes have edges / Edges have Capacity + Prop Delay tuple
+
+
+#Graph stores 
+# - List of nodes (each node will know about its neighbours)
+# - List of edges (no duplicates) , (edges will have Dprop,MaxCap,CurrCap)
+
+class Vertex:
+    def __init__(self, node):
+        self.id = node
+        self.adjacent = {}
+
+    def __str__(self):
+        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
+
+    def add_neighbor(self, neighbor, weight=0):
+        self.adjacent[neighbor] = weight
+
+    def get_connections(self):
+        return self.adjacent.keys()
+
+    def get_id(self):
+        return self.id
+
+    def get_weight(self, neighbor):
+        return self.adjacent[neighbor]
+
+class Graph:
+    def __init__(self):
+        self.vert_dict = {}
+        self.num_vertices = 0
+
+    def __iter__(self):
+        return iter(self.vert_dict.values())
+
+    def add_vertex(self, node):
+        self.num_vertices = self.num_vertices + 1
+        new_vertex = Vertex(node)
+        self.vert_dict[node] = new_vertex
+        return new_vertex
+
+    def get_vertex(self, n):
+        if n in self.vert_dict:
+            return self.vert_dict[n]
+        else:
+            return None
+
+    def add_edge(self, frm, to, cost = 0):
+        if frm not in self.vert_dict:
+            self.add_vertex(frm)
+        if to not in self.vert_dict:
+            self.add_vertex(to)
+
+        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
+        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
+
+    def get_vertices(self):
+        return self.vert_dict.keys()
 
 #Just some simple error-checking in args
 if(len(sys.argv) != 6): print "Usage: python RoutingPerformance.py <CIRCUIT/PACKET> <SHP/SDP/LLP> <topology-file> <workload-file> <rate>"; exit()
@@ -15,6 +75,13 @@ if(int(sys.argv[5]) < 1): print "Rate must be positive non-zero integer"; exit()
 try:
 	top = open(sys.argv[3]);	work = open(sys.argv[4])
 except Exception as e: print str(e); exit()
+
+#parse topology file into a list 
+
+
+#parse workload file into a list
+
+
 
 #the type of network scheme defines the behaviour of the algorithms ? 
 
