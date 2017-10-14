79b72cabe7b29716dabf246ffe0756c8f6f089cb
z5087077 <z5087077@student.unsw.edu.au>
2017-10-15 01:51:36 +1100

    Made changes to how we handle graphs Let me know what you tink will start work on the algorithms soon

--- RoutingPerformance.py
+++ RoutingPerformance.py
@@ -8,103 +8,46 @@ import sys, re
 #Code referrenced from: http://www.bogotobogo.com/python/python_graph_data_structures.php
 #Graph has nodes / Nodes have edges / Edges have Capacity + Prop Delay tuple
 
+'''
+So I decided its better to not make a separate Graph object because dealing with duplicates is a pain in the ass
+Instead I propose we use this way of using graphs instead 
+- Dictionary of dictionaries to represent fields in edges  (edges are the keys)
+- eg.  Graph = {}
+       Graph["AB"] = {"delay":10,"max",5,"load",0}
 
-#Graph stores 
-# - List of nodes (each node will know about its neighbours)
-# - List of edges (no duplicates) , (edges will have Dprop,MaxCap,CurrCap)
+       then we can access these fields easily 
+       if we want to get/set the load of edge AB just do 
 
-class Vertex:
-    def __init__(self, node):
-        self.id = node
-        self.neighbours = []
+       ABLoad = Graph["AB"]["load"]
+       or  Graph["AB"]["load"] = newABLoad
 
-    def __str__(self):
-        return str(self.id) + ' neighbours: ' + str([x.id for x in self.neighbours])
+       if we need to add new fields simply do
 
-    def addNeighbour(self, neighbour):
-    	if neighbour not in self.neighbours: self.neighbours.append(neighbour)
+       Graph["AB"]["newFIeld"] = newValue 
 
-    def getNeighbours(self): return self.neighbours
-    def getID(self): return self.id
+Some issues that we have to deal with (but should be easily done)
+Edges may come in wrong order eg. AB or BA (but we can have some convention like stricly sorted alphabetically edges)
 
-class Edge:
-	def __init__(self,node1,node2,delay,max):
-		self.node1 = node1
-		self.node2 = node2
-		self.delay = delay
-		self.maxCapacity = max
-		self.load = 0
+How do we find edges based on vertex ? eg. given C I need to consider all edges like AC , CD , CE,  FC .... (again see above)
+- to solve this we can just write a function to do Graph.keys() and return a list of all keys which contain "C" 
+'''
 
-	def isFull(self):
-		return (self.maxCapacity == self.load)
+#returns the correct order of edge eg. "AB" -> "AB" , "BA" -> "AB"
+def reorder(one,two): return (one + two) if (ord(one) < ord(two)) else (two + one);
 
-	def addLoad(self,load):
-		if(not isFull(self)): self.load = self.load + load
+#return list of edges that involve that node
+def getEdges(node,g): return [edge for edge in g if node in edge]
 
-	def reduceLoad(self,load):
-		if(self.load > 0): self.load = self.load - load
+def ShortestHop():
+	print("Shortest Hop Path !")
 
-	def availableSpace(self):
-		return (self.maxCapacity - self.load)
+def ShortestDelay():
+	print("Shortest Delay Path !")
 
-	def getDelay(self): return self.delay
-	def getLoad(self): return self.load
-	def getMaxCapacity(self): return self.maxCapacity
-	def getNode1(self): return self.node1
-	def getNode2(self): return self.node2
-
-	def isNode(self,node): return ((node == self.node1) or (node == self.node2))
+def LeastLoad():
+	print("Least Load Path !")
 
 
-class Graph:
-    def __init__(self):
-        self.edges = []
-        self.vertices = []
-
-    def addVertex(self, node):
-        new = Vertex(node)
-
-        #Check if the same node exists in the list or not
-        for v in self.vertices:
-        	if(v.getID() == node): return new
-
-        self.vertices.append(new)
-        return new
-
-    def addNeighbour(self, vertex, neighbour):   #?
-
-    	node1 = self.getVertex(vertex)
-    	node2 = self.getVertex(neighbour)
-    	for v in self.vertices:
-    		if(v.getID() == node1.getID()):
-    			v.addNeighbour(node2)
-    		if(v.getID() == node2.getID()):
-    			v.addNeighbour(node1)
-
-    def getVertex(self, n):
-        for v in self.vertices:
-            if (v.getID() == n): return v
-
-        return None
-
-    def addEdge(node1, node2, delay, max):
-        if frm not in self.vert_dict:
-            self.add_vertex(frm)
-        if to not in self.vert_dict:
-            self.add_vertex(to)
-
-        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
-        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
-
-    def getVertices(self):
-    	ret = [];
-    	for v in self.vertices:
-    		ret.append(v.getID());
-        return ret
-
-    def show(self):
-    	print "Vertices :" + str(self.getVertices())
-    	print "Edges    :" + str(self.edges)
 
 #Just some simple error-checking in args
 if(len(sys.argv) != 6): print "Usage: python RoutingPerformance.py <CIRCUIT/PACKET> <SHP/SDP/LLP> <topology-file> <workload-file> <rate>"; exit()
@@ -118,45 +61,49 @@ try:
 	work = open(sys.argv[4])
 except Exception as e: print str(e); exit()
 
+Graph = {} #Graph is a dict containing dicts
+scheme = sys.argv[1]
+algorithm = sys.argv[2]
 
 #Graph initialization
-g = Graph()
 for line in top.readlines():
 	match = re.search("([A-Z]) ([A-Z]) (\d+) (\d+)",line)
 	
-	node1 = match.group(1)  #just a character like "A"
-	node2 = match.group(2)  #just a character like "B" 
-	dprop = match.group(3)
+	edge = reorder(match.group(1), match.group(2))
+	propDelay = match.group(3)
 	maxCapacity = match.group(4)
 
-	#Add node to graph if it doesn't exist
-	g.addVertex(node1); g.addVertex(node2)
+	Graph[edge] = {"delay":propDelay, "max":maxCapacity, "load":0}
 
-	#Add edge in the graph
-	#g.addEdge(node1,node2,dprop,maxCapacity)
-
-	#Add to each other's neighbours
-	g.addNeighbour(node1,node2)
-
-	#According to the format of the sample the input won't have duplicates so all good
+	print str(edge) + " -> " + str(Graph[edge])  #EXAMPLE , DELETE LATER
 
 #Finish graph initialization
-g.show()
 top.close()
+
+print "\nCan we find C ?"        # ------------------------
+myList = getEdges("C",Graph)   #  EXAMPLE , DELETE LATER
+print myList				   # ------------------------
+
 
 #Parse workload file 
 for line in work.readlines():
 	match = re.search("(\d+) ([A-Z]) ([A-Z]) (\d+)",line)
 
 	time = match.group(1)
-	requester = match.group(2)
-	recipient = match.group(3)
+	targetEdge = reorder(match.group(2), match.group(3))
 	duration = match.group(4)
 
-#the type of network scheme defines the behaviour of the algorithms ? 
+	#the type of network scheme defines the behaviour of the algorithms ? 
 
-#Choose which routing algorithm to run
+	#Choose which routing algorithm to run
+	if(algorithm == "SHP"): 
+		ShortestHop()
+	elif(algorithm == "SDP"): 
+		ShortestDelay()
+	else:
+		LeastLoad()
+
+
 
 
 work.close()
-
