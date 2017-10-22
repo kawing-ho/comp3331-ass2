07b2ed4527c9184cc80e4a207a2a24d560501524
z5087077 <z5087077@student.unsw.edu.au>
2017-10-23 00:43:44 +1100

    Final version (i hope)

--- RoutingPerformance.py
+++ RoutingPerformance.py
@@ -4,9 +4,8 @@
 #z5113471 Andy Yang
 
 '''
-References :
-(1)> List-sorting with tuples in them: https://stackoverflow.com/a/36075587
-(2)> ...
+#(1)> List-sorting with tuples in them: https://stackoverflow.com/a/36075587
+#(2)> ...
 '''
 
 import sys, re, time, math, json
@@ -40,7 +39,6 @@ def tuple_compare(a, b):
 		else: return -1
 	else: return 1
 #-----------------------------------
-
 
 #check if a path has already been computed before for two nodes
 #input: a list of edges
@@ -78,44 +76,6 @@ def updateGraph(path, value):
 #output: sum of all delay in the list
 def countDelay(path):
 	return sum( [ int(Graph[edge]['delay']) for edge in path ] )
-
-#SHP algorithm
-#Weight = 1 (same across all edges)
-#input : Source, Dest, Graph
-#output: Path as a list from Src -> Dest
-def shortestHop(source, dest, graph):
-	path = {}						#Path is dict going backwards eg. path[B] = A
-	PriorityQueue = [(source,0)];   #PQ stores tuple of (node,distanceFromSource)
-	visited = set()
-
-	while True:
-
-		currentNode, currentDistance = PriorityQueue.pop(0)		#get node from PQ
-		visited.add(currentNode)								#add node to visited
-
-		if(currentNode == dest): break							#break if goal found
-
-		# look at neighbours and consider weights
-		for neighbour in getNeighbours(currentNode,graph):
-			if neighbour in visited: continue					#skip if seen neighbour before
-			else: visited.add(neighbour)
-
-			PriorityQueue.append((neighbour,currentDistance + 1)) #weight = 1 for all edges in SHP
-
-			path[neighbour] = currentNode
-
-		#reshuffle PQ (sort by distance)
-		PriorityQueue.sort(tuple_compare)
-
-	res = []
-	node = dest
-	while True:
-		res = [node] + res
-		if node in path: node = path[node]
-		else : break
-
-
-	return res
 
 
 #Main algorithm for computing routes (depending on algorithm)
@@ -177,144 +137,14 @@ def dijkstra(source, dest, graph):
 
 	return res
 
-
-def SHPTest():
-	print "=== Testing out SHP with A and D ==="
-	#print str(shortestHop('A', 'D', Graph))
-	print str(dijkstra('A', 'D', Graph))
-	print "=== Testing out SHP A and B ==="
-	#print str(shortestHop('A', 'B', Graph))
-	print str(dijkstra('A', 'B', Graph))
-	print "=== Testing out SHP D and C ==="
-	#print str(shortestHop('D', 'C', Graph))
-	print str(dijkstra('D', 'C', Graph))
-	print "=== Testing out SHP D and A ==="
-	#print str(shortestHop('D', 'A', Graph))
-	print str(dijkstra('D', 'A', Graph))
-
-
-	if('G' not in getNodes(Graph)): return
-	print " *** Topology.txt ***"
-	print "=== Testing out SHP G and I ==="
-	#print str(shortestHop('G', 'I', Graph))
-	print str(dijkstra('G', 'I', Graph))
-	print "=== Testing out SHP I and G ==="
-	#print str(shortestHop('I', 'G', Graph))
-	print str(dijkstra('I', 'G', Graph))
-	print "=== Testing out SHP D and O ==="
-	#print str(shortestHop('D', 'O', Graph))
-	print str(dijkstra('D', 'O', Graph))
-	print "=== Testing out SHP M and E ==="
-	#print str(shortestHop('M', 'E', Graph))
-	print str(dijkstra('M', 'E', Graph))
-
-def SDPTest():
-	print "************Testing SDP******************"
-	print "\n========A to D=============="
-	print str(dijkstraSDP('A', 'D', Graph))
-	print "\n========A to E=============="
-	print str(dijkstraSDP('A', 'E', Graph))
-	print "\n========F to C=============="
-	print str(dijkstraSDP('F', 'C', Graph))
-	print "\n========E to C=============="
-	print str(dijkstraSDP('E', 'C', Graph))
-	print "\n========F to O=============="
-	print str(dijkstraSDP('F', 'O', Graph))
-	print "\n========K to L=============="
-	print str(dijkstraSDP('K', 'L', Graph))
-	print "\n========I to G=============="
-	print str(dijkstraSDP('I', 'G', Graph))
-	print "\n========M to A=============="
-	print str(dijkstraSDP('M', 'A', Graph))
-	print "\n========E to I=============="
-	print str(dijkstraSDP('E', 'I', Graph))
-
-	print "***************Testing dijkstra**************"
-	print "\n========A to D=============="
-	print str(dijkstra('A', 'D', Graph))
-	print "\n========A to F=============="
-	print str(dijkstra('A', 'F', Graph))
-	print "\n========A to E=============="
-	print str(dijkstra('A', 'E', Graph))
-	print "\n========F to C=============="
-	print str(dijkstra('F', 'C', Graph))
-	print "\n========E to C=============="
-	print str(dijkstra('E', 'C', Graph))
-	print "\n========A to A=============="
-	print str(dijkstra('A', 'A', Graph))
-	print "\n========F to O=============="
-	print str(dijkstra('F', 'O', Graph))
-	print "\n========K to L=============="
-	print str(dijkstra('K', 'L', Graph))
-	print "\n========I to G=============="
-	print str(dijkstra('I', 'G', Graph))
-	print "\n========M to A=============="
-	print str(dijkstra('M', 'A', Graph))
-	print "\n========E to I=============="
-	print str(dijkstra('E', 'I', Graph))
-
-def LLPTest():
-	return None
-
-
 def getDelayOfEdge(edge): return int(Graph[edge]['delay'])
 def getLoadOfEdge(edge): return int(Graph[edge]['load'])
 def getCapacityOfEdge(edge): return int(Graph[edge]['max'])
 
-#Prototype for SDP
-#input : Source, Dest, Graph
-#output: Path as a list from Src -> Dest but using the shortest delay time
-def dijkstraSDP(source, dest, graph):
-	path = {}								#Path is dict going backwards eg. path[B] = A
-	PriorityQueue = [(source,0)];			#PQ stores tuple of (node,totalDelayFromSource)
-	visited = set()
 
-	while PriorityQueue:
-
-		#Get node + delay so far from top of PQ
-		currentNode, delayToCurrentNode = PriorityQueue.pop(0)
-
-		#Get neighbours of this node, also compute the proper weightages
-		for neighbour in getNeighbours(currentNode,graph):
-			if neighbour in visited: continue		#prevent looping / backtracking
-
-			currentEdge = reorder(currentNode,neighbour)
-			currentDelay = getDelayOfEdge(currentEdge)
-
-			#print neighbour,":Considering",currentEdge,"->",currentDelay,"from",currentNode,"with",delayToCurrentNode
-
-			comparativeDelay = currentDelay + delayToCurrentNode  		#value to compare with one on the queue
-			originalDelay = dict(PriorityQueue).get(neighbour, None)  	#value found on the queue (if any)
-
-			#if that node is already in the PriorityQueue do triangulation
-			if(originalDelay is not None):
-
-				#don't have to change anything if the originalDelay is less 
-				if(comparativeDelay < originalDelay):
-
-					#Change the originalDelay in PQ to the comparativeDelay
-					PriorityQueue.remove((neighbour,originalDelay))			#tuples can't be edited
-					PriorityQueue.append((neighbour,comparativeDelay))		#so have to remove and re-add again
-
-					#Change path to pass through current node instead
-					path[neighbour] = currentNode
-
-			else:
-				PriorityQueue.append((neighbour,delayToCurrentNode + currentDelay))
-				path[neighbour] = currentNode
-
-		PriorityQueue.sort(tuple_compare)			#reshuffle the PQ (sort by propDelay)
-		visited.add(currentNode)					#add currentNode to finished list
-
-	#reconstruct the path into a list
-	res = []
-	node = dest
-	while True:
-		res = [node] + res
-		if node in path: node = path[node]
-		else : break
-
-	return res
+#===================
+#   MAIN FUNCTION  
+#===================
 
 
 #Just some simple error-checking in args
@@ -331,7 +161,10 @@ except Exception as e: print str(e); exit()
 
 Graph = {} 						   #Graph is a dict containing dicts
 routes = {}						   #route is a dict containing pre-computed paths for chosen source/dest nodes
-scheme = sys.argv[1]
+jobQueue = []					   #queue of open and close events for connections
+activeRequests = []				   #list of activeRequests to keep track of what is open
+
+scheme = sys.argv[1]			   #unsued here since we did work on CIRCUIT
 algorithm = sys.argv[2]
 rate = int(sys.argv[5])            # eg. if rate = 2 it means 2 packets/s
 
@@ -342,6 +175,9 @@ CLOSE = 0
 #variables for printing statistics at the end
 blockedPackets = 0
 successfulPackets = 0
+totalHops = 0
+totalDelay = 0
+successfulCircuits = 0
 
 #Graph initialization
 for line in top.readlines():
@@ -355,25 +191,6 @@ for line in top.readlines():
 
 #Finish graph initialization
 top.close()
-
-#print json.dumps(Graph, indent=4)
-
-#Testing area
-if(algorithm == "SHP"): SHPTest()
-elif(algorithm == "SDP"): SDPTest()
-else: LLPTest()
-
-#-------------------------------------------------
-# ----------------WORK IN PROGRESS----------------
-#-------------------------------------------------
-
-jobQueue = []				#queue of open and close events for connections
-activeRequests = []
-totalHops = 0
-totalDelay = 0
-successfulCircuits = 0
-
-#Job structure: [TIME_OF_EVENT, SOURCE, DEST, DURATION, TYPE]
 
 startTime = 0.000000
 #Parse workload file into a job queue
@@ -435,13 +252,7 @@ while jobQueue:
 			if(fullCapacity(path)):
 				#add statistics
 
-				#print str(path),"is blocked"
-				#print sorted(activeRequests)
-				#print json.dumps(Graph,indent=4)
-
 				blockedPackets += numberOfPackets
-
-				#raw_input()
 
 			else:
 
@@ -462,16 +273,14 @@ while jobQueue:
 		# - If the request to open was not blocked decrease the load
 		#elif(type == CLOSE and circuitPath in activeRequests):
 		else:
+
+			#if the connection was blocked before don't close it !
 			if(circuitPath in activeRequests):
 				#remove from activeRequests
 				activeRequests.remove(circuitPath)
 
 				#decrease load across the path
 				updateGraph(path,-1)
-			#else:
-				#print circuitPath,"was blocked so no need to close : ",str(routes[circuitPath])
-				#print sorted(activeRequests)
-				#raw_input()
 
 	startTime += 0.000001
 
@@ -480,10 +289,10 @@ totalPackets = successfulPackets + blockedPackets
 
 #Print summary and statistics
 print "\nTotal number of virtual circuit requests:",(requestCount+1)
-print "Total number of packets:",totalPackets
-print "Number of successfully routed packets:",successfulPackets
+print "Total number of packets:",int(totalPackets)
+print "Number of successfully routed packets:",int(successfulPackets)
 print "Percentage of succesfully routed packets:",((float(successfulPackets)/totalPackets)*100),"%"
-print "Number of blocked packets:",blockedPackets
+print "Number of blocked packets:",int(blockedPackets)
 print "Percentage of blocked packets:",((float(blockedPackets)/totalPackets)*100),"%"
 print "Average number of hops per circuit:",(float(totalHops)/successfulCircuits)
 print "Average cumulative propagation delay per circuit:",(float(totalDelay)/successfulCircuits)
